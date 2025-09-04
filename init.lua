3305d63ec94036919f712acb04274b3198bd32c0
Mika Rollenmiller <mr@novacode.dev>
2025-09-04 14:50:51 +0200

    dev: Add move up and down via J and K

--- init.lua
+++ init.lua
@@ -180,6 +180,8 @@ vim.opt.colorcolumn = '100'
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('i', 'jj', '<Esc>')
 vim.keymap.set('n', '<leader>u', '<cmd>Telescope undo<cr>')
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
